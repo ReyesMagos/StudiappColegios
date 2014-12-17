21e70195ca520bdc422e9e9f4f19b031417c124d
alexis0592 <yefry.calderon@udea.edu.co>
2014-12-16 20:05:22 -0500

    adding spinner in actionBar

--- app/src/main/java/co/reyesmagos/studiappcolegios/fragments/TareasFragmentActivity.java
+++ app/src/main/java/co/reyesmagos/studiappcolegios/fragments/TareasFragmentActivity.java
@@ -4,9 +4,16 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import co.reyesmagos.studiappcolegios.R;
 import co.reyesmagos.studiappcolegios.fragments.adaptadores.CustomAdapterTareas;
@@ -19,25 +26,59 @@ public class TareasFragmentActivity extends Fragment {
 
     private View rootView;
     private ExpandableListView listView;
+    private MenuItem mItem = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         rootView = inflater.inflate(R.layout.tareas_activity, container, false);
+        rootView = inflater.inflate(R.layout.tareas_activity, container, false);
+        setHasOptionsMenu(true);
         initComponents();
         return rootView;
     }
 
-    public void initComponents(){
-        listView = (ExpandableListView)rootView.findViewById(R.id.expandableListView);
+    public void initComponents() {
+        listView = (ExpandableListView) rootView.findViewById(R.id.expandableListView);
 
-        listView.setIndicatorBounds(listView.getRight() - 60,listView.getWidth() - 8);
+        listView.setIndicatorBounds(listView.getRight() - 60, listView.getWidth() - 8);
         CustomAdapterTareas customAdapterTareas = new CustomAdapterTareas(rootView.getContext(), TareasFactory.getInstance());
         listView.setAdapter(customAdapterTareas);
 
         ActionBar actionBar = getActivity().getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+        actionBar.setDisplayShowTitleEnabled(false);
+
 
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(),
+                R.array.spinner_items, android.R.layout.simple_spinner_item);
+
+        menuInflater = getActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.tarea_menu_spinner, menu);
+        mItem = menu.findItem(R.id.menu_spinner);
+
+        View view = mItem.getActionView();
+        if (view instanceof Spinner) {
+            final Spinner spinner = (Spinner) view;
+            spinner.setAdapter(adapter);
+
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> adapterView) {
+
+                }
+            });
+        }
+
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
 }
